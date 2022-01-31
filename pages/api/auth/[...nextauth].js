cd462491df253a5ece162854c8dd5145ae41ec7e
samiullah <sami.ullah@fff>
2022-01-31 16:14:39 +0500

    tenth commit

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -13,5 +13,18 @@ export default NextAuth({
   pages:{
      signIn:"/auth/Login"
   },
+  callbacks:{
+    async session({session,token,user}) {
+      session.user.username=session.user.name.
+      split(" ")
+      .join("")
+      .toLocaleLowerCase();
+
+      session.user.uid=token.sub;
+      return session;
+
+    }
+  },
+
   secret:process.env.JWT_SECRET,
 })
